0e19c579595edc618725f4d0c4b3b57fc515bc79
Ahmad Samir <ahmadsamir2210@gmail.com>
2024-11-22 18:47:50 +0200

    Add API Versioning to auth APIs

--- users/apis/urls.py
+++ users/apis/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from users.apis import views
 
+api_v1_prefix = "api/v1"
+
 urlpatterns = [
-    path('auth/register/', views.RegisterView.as_view(), name='register'),
-    path('auth/login/', views.LoginView.as_view(), name='login'),
+    path(f'{api_v1_prefix}/auth/register/',
+         views.RegisterView.as_view(), name='register'),
+    path(f'{api_v1_prefix}/auth/login/',
+         views.LoginView.as_view(), name='login'),
 ]
