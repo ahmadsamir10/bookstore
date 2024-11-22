44d45adfa1b1b8c64c713f23396ff6b9ec3874cc
Ahmad Samir <ahmadsamir2210@gmail.com>
2024-11-22 03:52:49 +0200

    Add logging for executed queries in terminal

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,3 +148,27 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+
+# Logging
+
+if DEBUG:
+    import logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
